0f25c00c83db6a08a640a7f036ac490832d19b63
Madhur Gupta <guptamadhur96@gmail.com>
2020-10-07 18:07:08 +0530

    Solution

--- question-1/main.js
+++ question-1/main.js
@@ -1,19 +1,46 @@
 // feel free to add more test cases.
-// the format is as follows - 
+// the format is as follows -
 // [[numbers, targetSum], expectedOutput, errorMessage]
 
 const testCases = [
-	[[[5, 4, 10,7, 1, 9], 13], true, "Triplet Exists"],
-	[[[4, 2, 5, 8, 11, 23], 9], false, "Triplet does not exist"]
-]
+  [[[5, 4, 10, 7, 1, 9], 13], true, "Triplet Exists"],
+  [[[5, 1, 4], 10], true, "Triplet Exists"],
+  [[[4, 2, 5, 8, 11, 23], 9], false, "Triplet does not exist"],
+  [[[4, 2, 5, 8, 5, 23], 9], true, "Triplet Exist"],
+];
 
-function main(
-	numbers,
-	targetSum){
-	// complete this function
-	return true
-}
+const main = (numbers, targetSum) => {
+  // complete this function
+  numbers.sort(function (a, b) {
+    return a - b;
+  });
+  //three numners only
+  if (numbers.length === 3) {
+    if (numbers[0] + numbers[1] + numbers[2] == targetSum) {
+      return true;
+    }
+  } else {
+    let l, r;
+    for (let i = 0; i < numbers.length - 2; i++) {
+      l = i + 1;
+      r = numbers.length - 1;
+      while (l < r) {
+        const localSum = numbers[i] + numbers[l] + numbers[r];
+        if (localSum == targetSum) {
+          return true;
+        } else if (localSum < targetSum) {
+          l = l + 1;
+        } else {
+          //localSum > targetSum
+          r = r - 1;
+        }
+      }
+    }
+  }
+  return false;
+};
 
-testCases.forEach(([ input, expectedResult, message ]) => {
-	console.assert(main(...input) === expectedResult, message)
-})
+testCases.forEach(([input, expectedResult, message]) => {
+  console.assert(main(...input) === expectedResult, message);
+  // console.log(main(...input) === expectedResult, message);
+});
--- question-2/main.js
+++ question-2/main.js
@@ -1,23 +1,103 @@
 const testCases = [
-    [[[
+  [
+    [
+      [
         [-1, 1, 0, -1, 1],
         [1, 0, 1, -1, 1],
         [1, 0, 0, -1, 1],
-    ]], 2, "Tomatoes will be rotten in 2 days"],
-    [[[
+      ],
+    ],
+    2,
+    "Tomatoes will be rotten in 2 days",
+  ],
+  [
+    [
+      [
         [-1, 1, 0, -1, 1],
         [0, 0, 1, -1, 1],
         [1, 0, 0, -1, 1],
-    ]], -1, "All tomatoes cannot be rotten"]
-]
+      ],
+    ],
+    -1,
+    "All tomatoes cannot be rotten",
+  ],
+  [
+    [
+      [
+        [-1, 1, 0, 0, 1],
+        [0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+      ],
+    ],
+    -1,
+    "All tomatoes cannot be rotten",
+  ],
+];
 
+function main(tomatoGrid) {
+  // complete this function
+  if (!tomatoGrid || tomatoGrid.length === 0) return -1;
 
-function main(
-    tomatoGrid){
-    // complete this function
-    return -1
+  let fresh = 0;
+  let queue = [];
+  const count = () => {
+    //Count of fresh and Rotten
+    for (let row = 0; row < tomatoGrid.length; row++) {
+      for (let col = 0; col < tomatoGrid[0].length; col++) {
+        if (tomatoGrid[row][col] === -1) queue.push({ row, col });
+        else if (tomatoGrid[row][col] === 1) fresh++;
+      }
+    }
+  };
+
+  const markRotten = (row, col) => {
+    // mark neighbours as rotten
+    if (row > 0 && tomatoGrid[row - 1][col] === 1) {
+      tomatoGrid[row - 1][col] = -1;
+      queue.push({ row: row - 1, col });
+      fresh--;
+    }
+    if (col > 0 && tomatoGrid[row][col - 1] === 1) {
+      tomatoGrid[row][col - 1] = -1;
+      queue.push({ row, col: col - 1 });
+      fresh--;
+    }
+    if (row < tomatoGrid.length - 1 && tomatoGrid[row + 1][col] === 1) {
+      tomatoGrid[row + 1][col] = -1;
+      queue.push({ row: row + 1, col });
+      fresh--;
+    }
+    if (col < tomatoGrid[0].length - 1 && tomatoGrid[row][col + 1] === 1) {
+      queue.push({ row, col: col + 1 });
+      tomatoGrid[row][col + 1] = -1;
+      fresh--;
+    }
+  };
+
+  count();
+
+  if (fresh === 0) return 0;
+
+  let minutes = 0;
+  while (queue.length > 0) {
+    // for every cycle, we empty the whole queue
+    let size = queue.length;
+    for (let i = 0; i < size; i++) {
+      // for each rotten tomato
+      let { row, col } = queue.shift(); //Remove first element from queue
+      markRotten(row, col);
+    }
+
+    // cycle is over, queue contains the next rotten tomato
+    minutes++;
+  }
+
+  // check there are still fresh tomato
+  if (fresh > 0) return -1;
+  return minutes - 1;
 }
 
-testCases.forEach(([ input, expectedResult, message ]) => {
-    console.assert(main(...input) === expectedResult, message)
-})
+testCases.forEach(([input, expectedResult, message]) => {
+  console.assert(main(...input) === expectedResult, message);
+  // console.log(main(...input) === expectedResult, message);
+});
